a0ce13babcb2114212e125bc9a99d1e26b784c6c
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-09-09 00:25:39 +0530

    Setting up basic marko compilation with test file

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ MMPilot is a minimal modern static website generator powered by [Node.js](https:
 **Currently under development. Things may change without notice.**
 
 ## License
+
 Licensed under the [MIT License](https://github.com/kunruch/mmpilot/blob/master/LICENSE.md)
--- index.js
+++ index.js
@@ -3,13 +3,34 @@
 /**
  * Module dependencies.
  */
- var program = require('commander');
- var pkg = require('./package.json');
+var program = require('commander');
+var fs = require('fs');
+var pkg = require('./package.json');
+var markoc = require('marko/compiler');
 
- program
-   .version(pkg.version)
-   .option('-b, --build', 'Builds the project')
-   .parse(process.argv);
+markoc.configure({ writeToDisk: false, preserveWhitespace:true });
 
- if (program.build) console.log('Building..');
- console.log('Done');
+program
+    .version(pkg.version)
+    .option('-b, --build', 'Builds the project')
+    .parse(process.argv);
+
+// The following line installs the Node.js require extension
+// for `.marko` files. Once installed, `*.marko` files can be
+// required just like any other JavaScript modules.
+require('marko/node-require').install();
+
+if (program.build) {
+    console.log('Building..');
+    var template = require('./web/index.marko');
+    var out = fs.createWriteStream('public/index.html', {
+        encoding: 'utf8'
+    });
+
+    // Render the template to 'index.html'
+    template.render({
+        name: 'Frank',
+        count: 30
+    }, out);
+}
+console.log('Done');
