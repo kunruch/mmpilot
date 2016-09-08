f934c6c49065f7d132d5db5d5304a673290ef8e6
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-09-08 21:34:52 +0530

    Adding commander and command line parsing bootstrap

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log('Hello, world!');
+/**
+ * Module dependencies.
+ */
+ var program = require('commander');
+ var pkg = require('./package.json');
+
+ program
+   .version(pkg.version)
+   .option('-b, --build', 'Builds the project')
+   .parse(process.argv);
+
+ if (program.build) console.log('Building..');
+ console.log('Done');
