805610250027a4063f3fdaa5cf3ada2a0c176c99
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-09-10 02:17:54 +0530

    Fixing logic for copying assets

--- commands/build.js
+++ commands/build.js
@@ -17,16 +17,13 @@ exports.execute = function() {
 
 
     // Copy Asset files
-    var asset_src_path = path.join(global.config.project_root, global.config.project.assets);
-    var asset_dest_path = path.join(dest_path, global.config.project.assets);
+    var asset_src_path = path.join(global.config.project_root, global.config.project.assets, "*");
 
     // Generate destination folder
-    logger.log('Generating directory : ' + asset_dest_path);
     shell.mkdir('-p', dest_path);
-    shell.mkdir('-p', asset_dest_path);
 
-    logger.log('Copying assets from : ' + asset_src_path + " to : " + asset_dest_path);
-    shell.cp('-r', path.join(asset_src_path), asset_dest_path);
+    logger.log('Copying assets..');
+    shell.cp('-r', asset_src_path, dest_path);
 
     // Generate HTML
     logger.info('Building HTML..');
