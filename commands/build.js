5f2f3097dac18e25cb400ab32c49c1890ed8659a
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-09-28 00:33:46 +0530

    Removing overall build time

--- commands/build.js
+++ commands/build.js
@@ -1,16 +1,12 @@
-var logger = require('./../lib/logger');
 var path = require('path');
 
 //Tasks required by this command
 var tasks = [require('./../tasks/assets'), require('./../tasks/html'), require('./../tasks/styles'), require('./../tasks/scripts')];
 
 exports.execute = function() {
-    logger.start("Build");
-
+  
     tasks.forEach(function(task) {
       task.init();
       task.processAll();
     });
-
-    logger.end("Build");
 };
