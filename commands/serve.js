dd486aac1d57bae182cdd54cc4e9af4eb0390499
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-10-06 20:24:40 +0530

    Adding support for specifying proxy server and delay in initial launch

--- commands/serve.js
+++ commands/serve.js
@@ -23,36 +23,48 @@ exports.execute = function() {
 		});
 
     // Start the server
-    browserSync.init({
-        	server: config.html.dest,
-					middleware: [require("connect-logger")()]
-		    },
-				function (err, bs) {
-					var status = 404;
-					var file = path.join(config.html.dest, "404.html");
+    if(config.serve.proxy.length > 0) {
+        if(config.serve.delay > 0) {
+          logger.info("Waiting for " + config.serve.delay + "ms");
+        }
+        setTimeout(function() {
+          browserSync.init({
+            proxy: config.serve.proxy
+          });
+        }, parseInt(config.serve.delay));
+    }
+    else {
+      browserSync.init({
+          	server: config.html.dest,
+  					middleware: [require("connect-logger")()]
+  		    },
+  				function (err, bs) {
+  					var status = 404;
+  					var file = path.join(config.html.dest, "404.html");
 
-					//In case of app mode, we will serve index.html
-					if(config.serve.mode == 'app') {
-						status = 200;
-						file = path.join(config.html.dest, "index.html");
-					}
+  					//In case of app mode, we will serve index.html
+  					if(config.serve.mode == 'app') {
+  						status = 200;
+  						file = path.join(config.html.dest, "index.html");
+  					}
 
-					bs.addMiddleware("*", function (req, res) {
-						fs.stat(file, function(err, stat) {
-						    if(err == null) {
-						        // file exists
-										res.writeHead(status, {"Content-Type": "text/html"});
-										fs.createReadStream(file).pipe(res);
-						    } else if(err.code == 'ENOENT') {
-						        // file does not exist
-										res.writeHead(404, {"Content-Type": "text/plain"});
-										res.write("Not Found: " + req.url);
-										res.end();
-						    } else {
-						        logger.error('Error while serving files: ', err.code);
-						    }
-						});
+  					bs.addMiddleware("*", function (req, res) {
+  						fs.stat(file, function(err, stat) {
+  						    if(err == null) {
+  						        // file exists
+  										res.writeHead(status, {"Content-Type": "text/html"});
+  										fs.createReadStream(file).pipe(res);
+  						    } else if(err.code == 'ENOENT') {
+  						        // file does not exist
+  										res.writeHead(404, {"Content-Type": "text/plain"});
+  										res.write("Not Found: " + req.url);
+  										res.end();
+  						    } else {
+  						        logger.error('Error while serving files: ', err.code);
+  						    }
+  						});
 
-					});
+  					});
 				});
+      }
 }
